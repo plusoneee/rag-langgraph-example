67275f29b1c152521be10b4094c279e02ba8bde7
plusoneee <y02121148s@gmail.com>
2025-01-15 10:54:33 +0800

    adjust example

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-from uuid import uuid4
 from graph.graph import app
-from langchain_core.messages import AIMessage
 
 if __name__ == "__main__":
-    thread_uuid = str(uuid4())
+    joy_config = {"configurable": {"thread_id": "joy"}}
+    bob_config = {"configurable": {"thread_id": "bob"}}
 
-    config = {"configurable": {"thread_id": thread_uuid}}
-    print("Hello, What can I do for you? (type 'q' or 'exit' to quit)")
-    
-    while True:
-        _input = input("You: ")
+    msg = "hello my name is joy"
+    response = app.invoke({"history": [msg]}, joy_config)
+    print("Joy:", msg, "\nAI: ", response["history"][-1].content)
+    msg = "what is my name?"
+    response = app.invoke({"history": [msg]}, joy_config)
+    print("Joy:", msg, "\nAI: ", response["history"][-1].content)
 
-        if _input == "q" or _input == "exit":
-            print("AI: goodbye!")
-            break
-
-        for event in app.stream({"history": _input}, config, stream_mode="values"):
-            if event.get("history") and isinstance(event["history"][-1], AIMessage):
-                print("AI:", event["history"][-1].content)
+    msg = "what is my name?"
+    response = app.invoke({"history": [msg]}, bob_config)
+    print("\nBob:", msg, "\nAI: ", response["history"][-1].content)
